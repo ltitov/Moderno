94396eb0af8afa30fa859d6b1fe00c56ac9a66b7
Ltitov <itsitou88@gmail.com>
2020-07-21 11:35:28 +0300

    added slider section

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,19 @@
 $(function () {
 
-var mixer = mixitup('.products__inner-box');
+  $(".rate-star").rateYo({
+    rating: 5,
+    starWidth: "13px",
+    readOnly: true
+  });
+
+  $('.product-slider__inner').slick({
+    dots: true,
+    arrows: false,
+    slidesToShow: 4,
+    slidesToScroll: 4
+  });
+
+  var mixer = mixitup('.products__inner-box');
 
 
 });
