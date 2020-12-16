a024cc84630832255a2ebae2b65db8620d0d9f0a
Artur Tsitou <itsitou88@gmail.com>
2020-12-16 15:12:45 +0300

    Сделал урок 12.1 "New items"

--- app/js/main.js
+++ app/js/main.js
@@ -2,7 +2,7 @@ $(function () {
 
   $(".rate-star").rateYo({
     rating: 5,
-    starWidth: "13px",
+    starWidth: "12px",
     readOnly: true
   });
 
@@ -11,6 +11,15 @@ $(function () {
     arrows: false,
     slidesToShow: 4,
     slidesToScroll: 4
+  });
+
+  $(".js-range-slider").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 1000,
+    from: 0,
+    to: 600,
+    prefix: "$"
   });
 
   var mixer = mixitup('.products__inner-box');
