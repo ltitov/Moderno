94eaec07475c94bbcdbe136300597c275d37537a
Artur i5 <itsitou88@gmail.com>
2021-03-15 13:24:09 +0300

    index page adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -10,7 +10,29 @@ $(function () {
     dots: true,
     arrows: false,
     slidesToShow: 4,
-    slidesToScroll: 4
+    slidesToScroll: 4,
+    responsive: [{
+        breakpoint: 1900,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        }
+      },
+      {
+        breakpoint: 1441,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 841,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+    ]
   });
 
   $(".js-range-slider").ionRangeSlider({
